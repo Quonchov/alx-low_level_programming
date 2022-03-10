faac816dd330a2d4cbfca5721f073e49c2a2396c
Chukwuemeka <ochiezechukwuemeka@gmail.com>
2022-03-10 09:16:21 -0500

    My grit is stronger than ever

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,13 @@
 #include <stdio.h>
-int main()
-{
-/*
- *Description: My grit is strong
+/**
+ * main - Program entry point
+ * Puts: Prints "Programming is like building a multilingual puzzle
+ * Newline added
+ *  *
+ * Return:  0 on success. Error code otherwise
  */
+int main(void)
+{
 puts("\"Programming is like building a multilingual puzzle");
 return (0);
 }
