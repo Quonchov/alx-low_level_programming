81b3d690d3a140c911c43a38f130d7954ef3b201
Chukwuemeka <ochiezechukwuemeka@gmail.com>
2022-03-10 10:16:15 -0500

    fafds

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,13 +1,13 @@
 #include <stdio.h>
 /**
  * main - Program entry point
- * Puts: Prints "Programming is like building a multilingual puzzle
+ * Printf: Prints "with proper grammar, but the outcome is a piece of art
  * Newline added
  *  *
  * Return:  0 on success. Error code otherwise
  */
 int main(void)
 {
-printf("with proper grammar, but the outcome is a piece of art");
+printf("with proper grammar, but the outcome is a piece of art,");
 return (0);
 }
