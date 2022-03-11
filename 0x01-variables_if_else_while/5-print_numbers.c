13adf08c4ec1a57b05b5004fefdbf20022866326
Chukwuemeka <ochiezechukwuemeka@gmail.com>
2022-03-11 16:14:35 -0500

    Numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - Prints the alphabet in lowercase
+ *
+ * Return: Alwasy 0 (success)
+ */
+int main(void)
+{
+	int num = 0;
+
+	while (num <= 10)
+	{
+		printf("%d\n", num);
+		num++;
+	}
+	return (0);
+}
