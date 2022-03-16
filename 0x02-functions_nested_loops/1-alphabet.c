2afa5b1adefb0557764fbd43c28139c59792897d
Chukwuemeka <ochiezechukwuemeka@gmail.com>
2022-03-15 21:32:47 -0400

    Betty

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * main - Prints the alphabet in lowercase
+ *
+ * Description: Prints lowercase alphabets
+ *
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+	char alphabet = 'a';
+
+	while (alphabet <= 'z')
+	{
+		_putchar (alphabet);
+		alphabet++;
+	}
+	_putchar ('\n');
+	return (0);
+}
