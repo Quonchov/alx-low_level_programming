9fd703acb54e8422c198502b38cfd4e50f1e65d1
Chukwuemeka <ochiezechukwuemeka@gmail.com>
2022-03-15 20:57:45 -0400

    Tasks

---
+++ 0x02-functions_nested_loops/tempCodeRunnerFile.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * main - print _putchar using put, include a newline too
+ *
+ * Description: prints _putchar
+ *
+ * Return: always 0 (success)
+ */
+
+int main(void)
+{
+	char c[9] = "_putchar";
+	int i = 0;
+
+	for (i = 0; i <= 1; i++)
+	{
+		_putchar(c[i]);
+	}
+	_putchar('\n');
+	return (0);
+}
